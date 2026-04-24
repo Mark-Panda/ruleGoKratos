46e210abf85acf18b41b658ee73d4df0fd442ef3
maxianfei <maxianfei@guanghe.tv>
2026-04-24 15:38:28 +0800

    add

--- mcp/mcp_server_tls/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-exec uv --directory ./ run mcp-server-tls -t "$TRANSPORT_TYPE"
--- mcp/mcp_server_tls/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-cd `dirname $0`
-
-cd src
-exec python3 -m mcp_server_tls.main -t streamable-http
